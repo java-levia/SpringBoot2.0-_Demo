124a2b3ce3386bcc3f41d8bcf6ba4a906954a4fb
Levia <Levia@163.com>
2018-11-22 10:34:45 +0800

    剔除不必要的提交

--- src/main/java/com/levia/eureka/SpringbootEurekaDemoApplication.java
+++ src/main/java/com/levia/eureka/SpringbootEurekaDemoApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-@SpringBootApplication
-@EnableEurekaServer
+@SpringBootApplication 
+@EnableEurekaServer  //表示这是一个注册中心
 public class SpringbootEurekaDemoApplication {
 
 	public static void main(String[] args) {
